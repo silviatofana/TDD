4c9fe7e33518ed40b74cf3878f4f22a88991e906
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-08-30 20:10:08 +0300

    correcting linters

--- solver.rb
+++ solver.rb
@@ -13,10 +13,10 @@ class Solver
   end
 
   def fizzbuzz(number)
-    return 'error' if number.class != Integer
-    return 'fizzBuzz' if (number % 15).zero?
-    return 'fizz' if (number % 3).zero?
-    return 'buzz' if (number % 5).zero?
+    return 'Error' if number.class != Integer
+    return 'FizzBuzz' if (number % 15).zero?
+    return 'Fizz' if (number % 3).zero?
+    return 'Buzz' if (number % 5).zero?
 
     number.to_s
   end
